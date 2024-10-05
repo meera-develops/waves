724c52e63f048666c1fb4918ee1951bb723985e7
lurrah <laurajbui2@gmail.com>
2024-10-05 00:43:00 -0400

    start getMusic

--- spotify-api/userInfo.js
+++ spotify-api/userInfo.js
@@ -1,13 +1,8 @@
-const userTok = localStorage.getItem('access_token');
-const response = await fetch('https://api.spotify.com/v1/me', {
-    headers: {
-    Authorization: 'Bearer ' + token
-    }
-});
 
 // name, followers, id
 async function getUser() {
     try {
+        const token = localStorage.getItem('access_token');
         const response = await fetch('https://api.spotify.com/v1/me', {
             headers: {
             Authorization: 'Bearer ' + token
