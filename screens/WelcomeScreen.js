8adec9961065f7cc48146b22b4b058917381aa2b
lurrah <laurajbui2@gmail.com>
2024-10-05 01:03:34 -0400

    Login Button

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const WelcomeScreen = () => {
     return (
         <View style={styles.container}>
-            <Text>Hello World!</Text>
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text style={styles.buttons}>
+                Log in
+              </Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -12,7 +16,23 @@ const WelcomeScreen = () => {
 const styles = StyleSheet.create({
     container: {
       marginTop: 50,
-    }
+    },
+    buttonContainer: {
+        top: 140,
+        backgroundColor: 'white',
+        borderColor: '#c8acd6',
+        padding: 15,
+        marginHorizontal: 25,
+        marginVertical: 20,
+        alignItems: 'center',
+        borderRadius: 20,
+        borderWidth: 10,
+      },
+      buttons: {
+        color: '#433D8B',
+        fontSize: 18,
+        fontWeight: 'bold'
+      }
 });
 
 export default WelcomeScreen;
